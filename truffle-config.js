b1dcf7422f772e5ac9e2a3108dfeb19a5b792c56
tmneth <80415416+tmneth@users.noreply.github.com>
2022-12-15 00:21:09 +0200

    Change truffle config to deploy contract on testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,34 @@
+require("dotenv").config();
+const { MNEMONIC, PROJECT_ID } = process.env;
+
 const path = require("path");
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*",
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: MNEMONIC,
+          providerOrUrl: `https://goerli.infura.io/v3/${PROJECT_ID}`,
+        }),
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
   },
-
   compilers: {
     solc: {
-      version: "0.8.11",
-      // docker: true,
-      // settings: {
-      //   optimizer: {
-      //     enabled: true,
-      //     runs: 200,
-      //   },
-      // },
+      version: "0.8.17",
     },
   },
 };
